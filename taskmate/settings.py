135fe089c25474b0f22400b21143134a8314116b
jayachandrab <jayachandra1805@gmail.com>
2020-08-22 18:15:20 +0530

    added gunicorn

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django-heroku
+import django_heroku
 
 import os
 from pathlib import Path
@@ -125,6 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
@@ -133,5 +137,7 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL="todolist"
 LOGIN_URL="login"
- 
- django_heroku.settings=(locals())
+
+django_heroku.settings(locals())
+
+
